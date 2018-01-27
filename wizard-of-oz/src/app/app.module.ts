605ef22da2ffb981c8b6006836cf0eade1e79a1d
Gianni Bombelli <bombelli@intre.it>
2018-01-27 18:37:08 +0100

    add angular material

--- wizard-of-oz/src/app/app.module.ts
+++ wizard-of-oz/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import {MaterialModule} from './module/material.modules';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ wizard-of-oz/src/app/module/material.modules.ts
@@ -0,0 +1,88 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+  MatNativeDateModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatMenuModule,
+  MatOptionModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatTableModule,
+  MatTooltipModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatOptionModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatToolbarModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatOptionModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatToolbarModule
+  ],
+  declarations: []
+})
+export class MaterialModule {
+}
