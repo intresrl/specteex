af51202d70d66b5808e28ced8eb6e945896ff84f
Gianni Bombelli <bombelli@intre.it>
2018-02-05 16:31:27 +0100

    add chat message input HTML/CSS

--- wizard-of-oz-client/src/app/component/chat/chat.component.ts
+++ wizard-of-oz-client/src/app/component/chat/chat.component.ts
@@ -1,10 +1,8 @@
-import {Component, Input} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component} from '@angular/core';
 
 import {Subject} from 'rxjs/Subject';
 
 import {WebSocketService} from '../../service/websocket.service';
-import {CustomErrorStateMatcher} from '../../service/form.service';
 
 @Component({
   selector: 'app-chat',
@@ -12,31 +10,12 @@ import {CustomErrorStateMatcher} from '../../service/form.service';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
-  @Input()
-  set webSocket(value: Subject<MessageEvent>) {
-    this._webSocket = value;
-  }
   private _webSocket: Subject<MessageEvent>;
 
   public message = 'connecting ...';
 
-  chatForm = new FormGroup({
-    message: new FormControl('', [
-      Validators.required
-    ])
-  });
-
-  matcher = new CustomErrorStateMatcher();
-
   constructor(private _webSocketService: WebSocketService) {
     this._webSocket = this._webSocketService.connect();
     this._webSocket.subscribe(messageEvent => this.message = messageEvent.data);
-  }
-
-  public sendMessage() {
-    if (this.chatForm.valid) {
-      const messageEvent: MessageEvent = new MessageEvent('worker', {data: this.chatForm.value});
-      this._webSocket.next(messageEvent);
-    }
   }
 }
--- wizard-of-oz-client/src/app/component/sidebar/sidebar.component.ts
+++ wizard-of-oz-client/src/app/component/sidebar/sidebar.component.ts
@@ -1,10 +1,39 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {CustomErrorStateMatcher} from '../../service/form.service';
+import {WebSocketService} from '../../service/websocket.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subject} from 'rxjs/Subject';
+import PerfectScrollbar from 'perfect-scrollbar';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterViewInit {
+  private _webSocket: Subject<MessageEvent>;
 
+  public ps: PerfectScrollbar;
+  chatForm = new FormGroup({
+    message: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  matcher = new CustomErrorStateMatcher();
+
+  constructor(private _webSocketService: WebSocketService) {
+    this._webSocket = this._webSocketService.connect();
+  }
+
+  ngAfterViewInit(): void {
+    this.ps = new PerfectScrollbar('#chat');
+  }
+
+  public sendMessage() {
+    if (this.chatForm.valid) {
+      const messageEvent: MessageEvent = new MessageEvent('worker', {data: this.chatForm.value});
+      this._webSocket.next(messageEvent);
+    }
+  }
 }
