15b0e07bfc127e2129e5015ac9a301246c59b7f1
Gianni Bombelli <bombelli@intre.it>
2018-02-12 23:49:51 +0100

    add all i3 users

--- wizard-of-oz-common/src/config/users-ui-config.ts
+++ wizard-of-oz-common/src/config/users-ui-config.ts
@@ -5,6 +5,16 @@ export const userUiConfigs = [
     _image: 'spectee.svg'
   },
   {
+    _email: 'aceti@intre.it',
+    _color: '#1fae9b',
+    _image: 'ace.png'
+  },
+  {
+    _email: 'ballabio@intre.it',
+    _color: '#1fae9b',
+    _image: 'blb.png'
+  },
+  {
     _email: 'bombelli@intre.it',
     _color: '#1fae9b',
     _image: 'san_2x.png'
@@ -15,9 +25,43 @@ export const userUiConfigs = [
     _image: 'cag_2x.png'
   },
   {
+    _email: 'chierichetti@intre.it',
+    _color: '#c997fe',
+    _image: 'chi.png'
+  },
+  {
+    _email: 'cricenti@intre.it',
+    _color: '#c997fe',
+    _image: 'cri.png'
+  },
+  {
+    _email: 'cruciani@intre.it',
+    _color: '#c997fe',
+    _image: 'cru.png'
+  },
+  {
     _email: 'duque@intre.it',
     _color: '#ff9d96',
     _image: 'duq_2x.png'
+  },  {
+    _email: 'volpi@intre.it',
+    _color: '#ff9d96',
+    _image: 'fox.png'
+  },
+  {
+    _email: 'ghislandi@intre.it',
+    _color: '#ff9d96',
+    _image: 'ghi.png'
+  },
+  {
+    _email: 'grieco@intre.it',
+    _color: '#ff9d96',
+    _image: 'gri.png'
+  },
+  {
+    _email: 'maggioni@intre.it',
+    _color: '#ff9d96',
+    _image: 'mag.png'
   },
   {
     _email: 'mantovani@intre.it',
@@ -30,9 +74,54 @@ export const userUiConfigs = [
     _image: 'mar_2x.png'
   },
   {
+    _email: 'mandelli@intre.it',
+    _color: '#ffc58c',
+    _image: 'mnd.png'
+  },
+  {
+    _email: 'mufatti@intre.it',
+    _color: '#ffc58c',
+    _image: 'mau.png'
+  },
+  {
+    _email: 'poggio@intre.it',
+    _color: '#ffc58c',
+    _image: 'pog.png'
+  },
+  {
+    _email: 'pozzoni@intre.it',
+    _color: '#ffc58c',
+    _image: 'poz.png'
+  },
+  {
+    _email: 'percivaldi@intre.it',
+    _color: '#ffc58c',
+    _image: 'prc.png'
+  },
+  {
     _email: 'ranica@intre.it',
     _color: '#ffb6d2',
     _image: 'ran_2x.png'
+  },
+  {
+    _email: 'rigillo@intre.it',
+    _color: '#ffb6d2',
+    _image: 'rig.png'
+  },
+  {
+    _email: 'rissoglio@intre.it',
+    _color: '#ffb6d2',
+    _image: 'ris.png'
+  },
+  {
+    _email: 'roggero@intre.it',
+    _color: '#ffb6d2',
+    _image: 'rog.png'
+  },
+  {
+    _email: 'rotondi@intre.it',
+    _color: '#ffb6d2',
+    _image: 'rot.png'
   },
   {
     _email: 'sacchi@intre.it',
@@ -40,8 +129,28 @@ export const userUiConfigs = [
     _image: 'sac_2x.png'
   },
   {
+    _email: 'salvi@intre.it',
+    _color: '#ffe697',
+    _image: 'sal.png'
+  },
+  {
     _email: 'santacroce@intre.it',
     _color: '#ff9d96',
     _image: 'san_2x.png'
+  },
+  {
+    _email: 'saracino@intre.it',
+    _color: '#ff9d96',
+    _image: 'sar.png'
+  },
+  {
+    _email: 'sironi@intre.it',
+    _color: '#ff9d96',
+    _image: 'sir.png'
+  },
+  {
+    _email: 'testa@intre.it',
+    _color: '#ff9d96',
+    _image: 'tes.png'
   }
 ];
